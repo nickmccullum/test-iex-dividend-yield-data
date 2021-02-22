9efd7155a2df763bba4f3b9a3992b45bf7b811e6
Nick McCullum <nicholasmccullum@gmail.com>
2021-02-22 11:25:39 -0400

    finalized the test script

---
+++ test.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import numpy as np
+from secrets import IEX_CLOUD_API_KEY
+
+stocks = pd.read_csv('wilshire_5000_stocks.csv', header=None)
+print(stocks)
+n = 100
+lists_of_stocks = np.array([stocks[0][q:q + n] for q in range(0, len(stocks[0]), n)])
+output_dataframe = pd.DataFrame()
+
+for stock_list in lists_of_stocks:
+    tickerString = ''
+    for a in stock_list:
+        tickerString = tickerString + a.strip() + ','
+    tickerString = tickerString[:-1]
+
+    batchAPICall = pd.read_json("https://cloud.iexapis.com/stable/stock/market/batch?symbols="+tickerString+"&types=stats,price,dividends&range=5y&cache=true&token="+IEX_CLOUD_API_KEY)
+    for stock in stock_list:
+        try:
+            dividend_yield = batchAPICall[stock]["stats"]["dividendYield"]
+            output_dataframe = output_dataframe.append(pd.Series([stock, dividend_yield]), ignore_index=True)
+        except KeyError:
+            pass
+        except TypeError:
+            pass
+
+output_dataframe.to_csv('iex_dividend_yield.csv')
